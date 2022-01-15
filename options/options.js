c1f4b94a467c15b1b03b8b3a7fa2ab828af1d15e
Crowdbotics <team@crowdbotics.com>
2022-01-15 12:06:50 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "solok40_by_kaleab_32857",
+  url: "https://solok40_by_kaleab_32857.botics.co",
+  api: "https://solok40_by_kaleab_32857.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
